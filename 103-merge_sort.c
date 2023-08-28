9b3d5266ea45a42c18efb0975c08d9564f2d28e5
Brook25 <bekelebrook24@gmail.com>
2023-08-28 16:38:08 +0000

    merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,11 +1,19 @@
 #include "sort.h"
 
-
+/**
+ * merge - merges two given sub arrays
+ * @array: array to be sorted
+ * @lb: lower bound of the sub arrays
+ * @mid: mid index of the subarrays to be mergged
+ * @ub: upper bound of the sub arrays
+ * Return: Nothing
+ */
 void merge(int array[], int lb, int mid, int ub)
 {
 	int i, left = lb, right = mid + 1, buf = 0;
 	int *buffer;
 	buffer = malloc(sizeof(int) * ((ub - lb) + 1));
+	
 	printf("[left]: ");
 	for (i = lb; i < mid; i++)
 		printf("%d, ", array[i]);
@@ -13,32 +21,47 @@ void merge(int array[], int lb, int mid, int ub)
 	printf("[right]: ");
 	for (i = mid + 1; i < ub; i++)
 		printf("%d, ", array[i]);
+	
 	printf("%d\n", array[i]);
 	while (left <= mid && right <= ub) {
 		if (array[left] < array[right]) {
 			buffer[buf] = array[left];
-			left++; }
+			left++;
+		}
 		else {
 			buffer[buf] = array[right];
-			right++; }
-		buf++; }
+			right++;
+		}
+		buf++;
+	}
 	while (left <= mid) {
 		buffer[buf] = array[left];
-		buf++; left++; }
+		buf++; left++;
+	}
 	while (right <= ub) {
 		buffer[buf] = array[right];
-		buf++; right++; }
+		buf++; right++; 
+	}
 	buf = 0; i = lb;
 	while (i <= ub) {
 		array[i] = buffer[buf];
-		i++; buf++; }
+		i++; buf++;
+       }
+	
 	printf("[Done]: ");
 	for (i = lb; i < ub; i++)
 		printf("%d, ", array[i]);
 	printf("%d\n", array[i]);
-	free(buffer); }
+	free(buffer); 
+}
 
-
+/**
+ * _merge_sort - does sorting of an array using merge sort algo
+ * @array: array to be sorted
+ * @lb: lower bound of sub arrays
+ * @ub: upper bound of sub arrays
+ * Return: Nothing
+ */
 void _merge_sort(int array[], int lb, int ub)
 {
 	int mid = (lb + ub) / 2;
@@ -52,7 +75,13 @@ void _merge_sort(int array[], int lb, int ub)
 	merge(array, lb, mid, ub);
 }
 
-
+/**
+ * merge_sort - calls helper function to sort an array
+ * using merge sort algo
+ * @array: array to be sorted
+ * @size: size of array
+ * Return: Nothing
+ */
 void merge_sort(int *array, size_t size)
 {
 	int lower_bound = 0, upper_bound = size - 1;
